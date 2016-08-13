03c83a67b3025a4de0664ecc176b228d8ea4b925
ptdtan <ptdtan@gmail.com>
2016-08-13 12:38:00 +0700

    dotplot code

--- dotplot.sh
+++ dotplot.sh
@@ -1,6 +1,6 @@
-s#!/bin/bash
+#!/bin/bash
 DELTA=$1
 grep ">" $DELTA > chroms
 sed -i 's/>//g' chroms
 echo "#!/bin/bash" >> plot.sh
-awk -F $" " '{print "mummerplot -r "$1" -q "$2" -p "$1"vs"$2" --png "${DELTA}"}' chroms > plot.sh
+awk -v DELTA="$DELTA" -F $" " '{print "mummerplot -r "$1" -q "$2" -p "$1"vs"$2" --png "DELTA}' chroms > plot.sh
